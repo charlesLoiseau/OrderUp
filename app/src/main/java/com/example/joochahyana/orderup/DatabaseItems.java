88965e476d3c1b9e4c99c610944610d3a9427e18
LeeKangJin <himnae5181@naver.com>
2018-06-08 11:00:19 +0900

    save a data in database.

--- app/src/main/java/com/example/joochahyana/orderup/DatabaseItems.java
+++ app/src/main/java/com/example/joochahyana/orderup/DatabaseItems.java
@@ -3,6 +3,7 @@ package com.example.joochahyana.orderup;
 import com.orm.SugarRecord;
 
 public class DatabaseItems extends SugarRecord {
+
     String              name;
     Double              price;
     String              description;
@@ -10,17 +11,18 @@ public class DatabaseItems extends SugarRecord {
 
     // Relationship
     DatabaseItemType    itemType;
+    DatabaseItemsImage  itemImage;
 
     public DatabaseItems() {
 
     }
 
-    public DatabaseItems(DatabaseItemType itemType, String name, Double price, String description, Integer stock) {
+    public DatabaseItems(DatabaseItemType itemType, String name, Double price, String description, Integer stock,DatabaseItemsImage itemImage) {
         this.itemType = itemType;
-
         this.name = name;
         this.price = price;
         this.description = description;
         this.stock = stock;
+        this.itemImage = itemImage;
     }
 }
--- app/src/main/java/com/example/joochahyana/orderup/FragmentStaffFoods.java
+++ app/src/main/java/com/example/joochahyana/orderup/FragmentStaffFoods.java
@@ -9,13 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentStaffFoods extends Fragment{
-    Button Addbutton;
-
-
+    Button addButton ,delButton;
+    int i=0;
     AdapterStaffFoods adapter;
 
     @Override
@@ -26,23 +27,22 @@ public class FragmentStaffFoods extends Fragment{
         adapter = new AdapterStaffFoods(getActivity(), arrayListStaffFoods);
 
 
-  /*
+         /*
         foodNumber = arrayListStaffFoods.size();
         for(int i=0; i<foodNumber; i++){
             adapter.add(arrayListStaffFoods.get(i));
 
+        }*/
+
+        // jo
+        List<DatabaseItems> dbItems = DatabaseItems.listAll(DatabaseItems.class);
+
+        for(int i=0; i<dbItems.size(); i++){
+            Foods tempFood =new Foods(dbItems.get(i).name,dbItems.get(i).description,dbItems.get(i).price,dbItems.get(i).itemImage.imageStream);
+            adapter.add(tempFood);
         }
-     */
-       Foods food1 = new Foods("Food Name 1", "Description of food 1", "$10.00", R.drawable.pic01,0);
-        adapter.add(food1);
-        Foods food2 = new Foods("Food Name 2", "Description of food 2", "$20.00", R.drawable.pic02,0);
-        adapter.add(food2);
-        Foods food3 = new Foods("Food Name 3", "Description of food 3", "$30.00", R.drawable.pic03,0);
-        adapter.add(food3);
-        Foods food4 = new Foods("Food Name 4", "Description of food 4", "$40.00", R.drawable.pic04,0);
-        adapter.add(food4);
-       Foods food5 = new Foods("Food Name 5", "Description of food 5", "$50.00", R.drawable.pic05,0);
-        adapter.add(food5);
+
+
 
     }
 
@@ -53,9 +53,9 @@ public class FragmentStaffFoods extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_staff_foods, container, false);
-        Addbutton = (Button)rootView.findViewById(R.id.buttonAdd);
+        addButton = (Button)rootView.findViewById(R.id.buttonAdd);
 
-        Addbutton.setOnClickListener(new Button.OnClickListener(){
+        addButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public  void onClick(View view){
                 Intent intent = new Intent(getActivity(),foodstaffActivity.class);
@@ -64,6 +64,25 @@ public class FragmentStaffFoods extends Fragment{
             }
 
         });
+
+
+        delButton = (Button)rootView.findViewById(R.id.buttonDel);
+
+        delButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public  void onClick(View view){
+
+
+                DatabaseItems.deleteAll(DatabaseItems.class);
+                DatabaseMenu.deleteAll(DatabaseMenu.class);
+                DatabaseMenuItem.deleteAll(DatabaseMenuItem.class);
+                DatabaseItemsImage.deleteAll(DatabaseItems.class);
+
+            }
+
+        });
+
+
 
         return rootView;
     }
